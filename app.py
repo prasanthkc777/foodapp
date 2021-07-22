e65b8db2ad05213519bfc35a9e61ecf2a1d4d3c3
prasanthkc777 <83226287+prasanthkc777@users.noreply.github.com>
2021-07-22 21:00:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import mysql.connector
-mydb=mysql.connector.connect(host="localhost",user="root",password="Gauranitai27#",database="fooddelivery")
+mydb=mysql.connector.connect(host="localhost",user="root",password="your_password",database="fooddelivery")
 mycursor=mydb.cursor()
     
 
